22248080bfe5e0551bab3f59b351140b341ca2f8
Paul Freeman <paul@level2d.com>
2019-12-12 14:12:56 -0500

    init

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,60 +1,62 @@
 import WebGLView from './webgl/WebGLView';
 
 export default class App {
+  constructor() {}
 
-	constructor() {
+  init() {
+    this.initWebGL();
+    this.addListeners();
+    this.animate();
+    this.resize();
+  }
 
-	}
+  initWebGL() {
+    this.webgl = new WebGLView(this);
+    document
+      .querySelector('.container')
+      .appendChild(this.webgl.renderer.domElement);
 
-	init() {
-		this.initWebGL();
-		this.addListeners();
-		this.animate();
-		this.resize();
-	}
+    this.webgl.renderer.domElement.id = 'fixedCanvas';
+  }
 
-	initWebGL() {
-		this.webgl = new WebGLView(this);
-		document.querySelector('.container').appendChild(this.webgl.renderer.domElement);
-	}
+  addListeners() {
+    this.handlerAnimate = this.animate.bind(this);
 
-	addListeners() {
-		this.handlerAnimate = this.animate.bind(this);
+    window.addEventListener('resize', this.resize.bind(this));
+    window.addEventListener('keyup', this.keyup.bind(this));
+  }
 
-		window.addEventListener('resize', this.resize.bind(this));
-		window.addEventListener('keyup', this.keyup.bind(this));
-	}
+  animate() {
+    this.update();
+    this.draw();
 
-	animate() {
-		this.update();
-		this.draw();
+    this.raf = requestAnimationFrame(this.handlerAnimate);
+  }
 
-		this.raf = requestAnimationFrame(this.handlerAnimate);
-	}
+  // ---------------------------------------------------------------------------------------------
+  // PUBLIC
+  // ---------------------------------------------------------------------------------------------
 
-	// ---------------------------------------------------------------------------------------------
-	// PUBLIC
-	// ---------------------------------------------------------------------------------------------
+  update() {
+    if (this.webgl) this.webgl.update();
+  }
 
-	update() {
-		if (this.webgl) this.webgl.update();
-	}
+  draw() {
+    if (this.webgl) this.webgl.draw();
+  }
 
-	draw() {
-		if (this.webgl) this.webgl.draw();
-	}
+  // ---------------------------------------------------------------------------------------------
+  // EVENT HANDLERS
+  // ---------------------------------------------------------------------------------------------
 
-	// ---------------------------------------------------------------------------------------------
-	// EVENT HANDLERS
-	// ---------------------------------------------------------------------------------------------
+  resize() {
+    if (this.webgl) this.webgl.resize();
+  }
 
-	resize() {
-		if (this.webgl) this.webgl.resize();
-	}
-
-	keyup(e) {
-
-		// r
-		if (e.keyCode == 82) { if (this.webgl.trackball) this.webgl.trackball.reset(); }
-	}
+  keyup(e) {
+    // r
+    if (e.keyCode == 82) {
+      if (this.webgl.trackball) this.webgl.trackball.reset();
+    }
+  }
 }
